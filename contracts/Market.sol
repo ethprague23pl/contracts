0507efa4a774e28f8607b4afa195754570743f35
Bartek <bartlomiej.lech97@gmail.com>
2023-06-10 14:20:57 +0200

    add market draft

---
+++ contracts/Market.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract Market is ReentrancyGuard {
+
+    function listTicket 
+    (
+        address eventAddress,
+        uint256 tokenId,
+        uint256 price
+    )
+    external
+    notListed(eventAddress, tokenId, msg.sender)
+    isOwner(eventAddress, tokenId, msg.sender) {
+        
+    }
+
+    function cancelListing(
+        address eventAddress,
+        uint256 tokenId
+    ) {
+
+    }
+
+    function buyTicker(
+        address eventAddress,
+        uint256 tokenId,
+    ) {
+
+    }
+
+    function getListing(
+        address eventAddress,
+        uint256 tokenId,
+    ) {
+
+    }
+}
