45eafb6f336d1574217015bc2ea7ba3ddcb6bc87
TomekSetner <tomek.setner@gmail.com>
2023-06-10 14:01:40 +0200

    init api methods

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,13 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
     .setTitle('api')
     .setDescription('api')
     .setVersion('1.0')
-    .addTag('api')
+    // .addTag('api')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
